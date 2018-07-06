7afe87160a2f208569e872a966665b9299f61925
Kamin, Grant <Grant.Kamin@garmin.com>
2018-07-06 09:04:10 -0500

    Use Stack View to Calculate Bounds
    Labels inside of stack views are not aligned properly
    because we were assuming leading alignment.
    Calculating the origin based off of the stack view alignment
    instead.

--- Sources/Extensions/UIView+Frame.swift
+++ Sources/Extensions/UIView+Frame.swift
@@ -12,6 +12,18 @@ import UIKit
 extension UIView {
     
     var maxBoundsEstimated: CGRect {
+        if let parentStackView = (superview as? UIStackView) {
+            var origin: CGPoint = .zero
+            switch parentStackView.alignment {
+            case .center:
+                origin.x = maxWidthEstimated / 2
+            case .trailing:
+                origin.x = maxWidthEstimated
+            default:
+                break
+            }
+            return CGRect(origin: origin, size: maxSizeEstimated)
+        }
         return CGRect(origin: .zero, size: maxSizeEstimated)
     }
     
