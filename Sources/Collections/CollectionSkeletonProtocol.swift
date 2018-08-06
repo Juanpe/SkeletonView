8988b2278448ecd7952a43e96e3c60c1de3ac618
Reece Como <reece.como@healthengine.com.au>
2018-08-06 11:09:20 +0800

    style consistency

--- Sources/Collections/CollectionSkeletonProtocol.swift
+++ Sources/Collections/CollectionSkeletonProtocol.swift
@@ -28,12 +28,6 @@ extension CollectionSkeleton where Self: UIScrollView {
     var estimatedNumberOfRows: Int { return 0 }
     func addDummyDataSource() {}
     func removeDummyDataSource(reloadAfter: Bool) {}
-    
-    func disableUserInteraction() {
-        isScrollEnabled = false
-    }
-    
-    func enableUserInteraction() {
-        isScrollEnabled = true
-    }
+    func disableUserInteraction() { isScrollEnabled = false }
+    func enableUserInteraction() { isScrollEnabled = true }
 }
