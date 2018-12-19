b364218213853299c5eae9174da67ced206c3f06
Juanpe Catalán <juan.catalan@odigeo.com>
2018-12-19 19:49:27 +0100

    feat: Add SkeletonConfig documentation

--- Sources/SkeletonConfig.swift
+++ Sources/SkeletonConfig.swift
@@ -2,12 +2,16 @@
 
 import UIKit
 
-/// Struct who contains the config needed to activate the skeleton layer
-/// in a view
+/// Used to store all config needed to activate the skeleton layer.
 struct SkeletonConfig {
+    /// Type of skeleton layer
     let type: SkeletonType
+    /// Colors used in skeleton layer
     let colors: [UIColor]
+    /// If type is gradient, which gradient direction
     let gradientDirection: GradientDirection?
+    /// Specify if skeleton is animated or not
     let animated: Bool
+    /// Used to execute a custom animation
     let animation: SkeletonLayerAnimation?
 }
