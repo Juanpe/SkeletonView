6784de9597b74982dbdd1c36d4c71c7161a59a1d
Guglielmo Faglioni <guglielmo@gorillatech.io>
2019-06-18 17:44:50 +0200

    fix - support UICollectionView's headers and footers (UICollectionReusableView)

--- Sources/Collections/CollectionViews/SkeletonCollectionViewProtocols.swift
+++ Sources/Collections/CollectionViews/SkeletonCollectionViewProtocols.swift
@@ -13,12 +13,19 @@ public protocol SkeletonCollectionViewDataSource: UICollectionViewDataSource {
     func numSections(in collectionSkeletonView: UICollectionView) -> Int
     func collectionSkeletonView(_ skeletonView: UICollectionView, numberOfItemsInSection section: Int) -> Int
     func collectionSkeletonView(_ skeletonView: UICollectionView, cellIdentifierForItemAt indexPath: IndexPath) -> ReusableCellIdentifier
+    func collectionSkeletonView(_ skeletonView: UICollectionView, supplementaryViewIdentifierOfKind: String, at indexPath: IndexPath) -> ReusableCellIdentifier?
 }
 
 public extension SkeletonCollectionViewDataSource {
     
     func collectionSkeletonView(_ skeletonView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return skeletonView.estimatedNumberOfRows
+    }
+    
+    func collectionSkeletonView(_ skeletonView: UICollectionView,
+                                supplementaryViewIdentifierOfKind: String,
+                                at indexPath: IndexPath) -> ReusableCellIdentifier? {
+        return nil
     }
     
     func numSections(in collectionSkeletonView: UICollectionView) -> Int { return 1 }
