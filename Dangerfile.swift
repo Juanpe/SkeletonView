dd6d5b0e7de5821b2a8dde83accbfa8ea7f68515
Juanpe Catalán <jpcatalan@tuenti.com>
2020-02-13 15:20:40 +0100

    update dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -7,6 +7,7 @@ extension Git {
 }
 
 let danger = Danger()
+//let pullRequest = danger.github.pullRequest
 
 SwiftLint.lint(.modifiedAndCreatedFiles(directory: "Sources"), inline: true)
 
@@ -14,5 +15,17 @@ SwiftLint.lint(.modifiedAndCreatedFiles(directory: "Sources"), inline: true)
 
 // Warn when there is a big PR
 if danger.git.linesOfCode > 1 {
+    markdown(" * [##{pr_number}](#{pr_url}): #{github.pr_title} - [@#{github.pr_author}](https://github.com/#{github.pr_author})")
     warn("Big PR, try to keep changes smaller if you can")
+}
+
+// Pull out the edited files and find ones that come from a sub-folder
+// where our app lives
+let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
+let editedAppFiles = editedFiles.filter { $0.contains("/Sources") }
+
+// Request for a CHANGELOG entry with each app change
+if editedAppFiles.count > 0 {
+    fail("Please add a CHANGELOG entry for these changes.")
+    markdown("")
 }
