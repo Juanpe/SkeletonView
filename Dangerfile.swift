744098d641addb7fbc504b2e52c7d63c4c145464
Juanpe Catalán <jpcatalan@tuenti.com>
2020-02-13 15:13:17 +0100

    update dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,2 +1,18 @@
-import Danger 
+import Danger
+
+extension Git {
+    var linesOfCode: Int {
+        createdFiles.count + modifiedFiles.count - deletedFiles.count
+    }
+}
+
 let danger = Danger()
+
+SwiftLint.lint(inline: true)
+
+// Warnings
+
+// Warn when there is a big PR
+if danger.git.linesOfCode > 20 {
+    warn("Big PR, try to keep changes smaller if you can")
+}
