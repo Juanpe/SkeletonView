7ce5e0623a7cb212139d596ca0a38095db5fd174
Josh Brunner <joshuaebrunner@gmail.com>
2021-02-08 14:06:32 -0800

    Fixed a deprecation warning where the AssociatedObjects protocol was inheriting from class instead of AnyObject per Xcode's suggestion (#376)

--- Sources/Helpers/AssociationPolicy.swift
+++ Sources/Helpers/AssociationPolicy.swift
@@ -17,7 +17,7 @@ enum AssociationPolicy: UInt {
     }
 }
 
-protocol AssociatedObjects: class { }
+protocol AssociatedObjects: AnyObject { }
 
 // transparent wrappers
 extension AssociatedObjects {
