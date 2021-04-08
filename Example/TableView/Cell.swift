61a6efbc7b977c68888b27346ff1507cd443a55f
Alex Lykesas <alexookah@me.com>
2021-04-08 12:59:32 +0300

    UnSwizzle methods when SkeletonView is hidden (#381)
    * Add InputAccessoryView to textField
    
    * Add method to removeOnce Add unSwizzleMethods that are called when recursiveHideSkeleton
    
    Co-authored-by: Alexandros Lykesas <alexandros.lykesas@untis.at>

--- Example/TableView/Cell.swift
+++ Example/TableView/Cell.swift
@@ -9,11 +9,25 @@
 import UIKit
 
 class Cell: UITableViewCell {
-    
+
     @IBOutlet weak var avatar: UIImageView!
     @IBOutlet weak var label1: UILabel!
-    
+    @IBOutlet weak var textField: UITextField!
+
     override func awakeFromNib() {
         super.awakeFromNib()
+        setUpInputAccessoryView()
+    }
+
+    func setUpInputAccessoryView() {
+        let bar = UIToolbar()
+        let reset = UIBarButtonItem(title: "InputAccessoryView", style: .plain, target: self, action: #selector(resetTapped))
+        bar.items = [reset]
+        bar.sizeToFit()
+        textField.inputAccessoryView = bar
+    }
+
+    @objc func resetTapped() {
+
     }
 }
