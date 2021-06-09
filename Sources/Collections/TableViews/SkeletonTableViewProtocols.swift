a06308fa134c4636d55a6964d8543b20c3e8c5e7
Samuel Harrison <samuel.j.harrison@gmail.com>
2021-06-09 18:47:00 +0100

    Fix reference to automaticNumberOfRows from pr #401

--- Sources/Collections/TableViews/SkeletonTableViewProtocols.swift
+++ Sources/Collections/TableViews/SkeletonTableViewProtocols.swift
@@ -16,7 +16,7 @@ public protocol SkeletonTableViewDataSource: UITableViewDataSource {
 
 public extension SkeletonTableViewDataSource {
     func collectionSkeletonView(_ skeletonView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Self.automaticNumberOfRows
+        return SkeletonCollectionDataSource.automaticNumberOfRows
     }
     
     func numSections(in collectionSkeletonView: UITableView) -> Int { return 1 }
