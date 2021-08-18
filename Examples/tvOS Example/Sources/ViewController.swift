89643e0d93c99854d53757aa42a21c1488580d96
Juanpe Catalán <juanpecm@gmail.com>
2021-08-18 13:19:51 +0200

    files reorganization (#432)

---
+++ Examples/tvOS Example/Sources/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tvOS Example
+//
+//  Created by Juanpe Catalán on 18/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SkeletonViewCore/Tests/DummyTests.swift
@@ -0,0 +1,7 @@
+    import XCTest
+    @testable import SkeletonView
+
+    final class DummyTests: XCTestCase {
+        func testExample() {
+        }
+    }
